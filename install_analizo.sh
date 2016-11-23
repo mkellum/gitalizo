700632f321f5cc454d9f6f985141adea5bce2aed
mkellum <kellumm@gmail.com>
2016-11-23 19:23:38 +0000

    correcting analizo install script

--- install_analizo.sh
+++ install_analizo.sh
@@ -2,4 +2,4 @@ echo deb http://www.analizo.org/download/ ./ >> /etc/apt/sources.list.d/analizo.
 echo deb-src http://www.analizo.org/download/ ./ >> /etc/apt/sources.list.d/analizo.list
 wget -O - http://www.analizo.org/download/signing-key.asc | apt-key add -
 apt-get update
-apt-get install analizo
+yes Y | apt-get install analizo
