ecf1bb11968f879630449615fcb53ef78954a1dc
mkellum <kellumm@gmail.com>
2016-11-24 10:37:31 -0600

    improved logging, removed cleanup, hardcoded repo list file, added repo index arg

--- gitalizo.py
+++ gitalizo.py
@@ -1,134 +1,113 @@
 # !/usr/bin/python
 
 import csv
-import os
 import subprocess
 import sys
 import time
 from urlparse import urlparse
 
-LOG_DIR = 'logs'
-CSV_DIR = 'csvs'
 
 def main():
-    make_dirs([LOG_DIR, CSV_DIR, Repo.ANALIZO_OUTS, Repo.REPO_TEMP])
-    start_log(LOG_DIR)
+    log = Log()
 
-    repo_list_file = sys.argv[1]
+    try:
+        repo_index = int(sys.argv[1])
+        if not repo_index or len(sys.argv) != 2:
+            raise Exception("Incorrect arguments: {}".format(sys.argv))
 
-    if not repo_list_file or len(sys.argv) != 2:
-        sys.exit("Usage: gitalizo <repo id filename>")
+        reader = csv.reader(open('repoList.csv', 'r'))
 
-    for repo_row in csv.reader(open(repo_list_file, 'r')):
-        try:
-            r = Repo(repo_row[0], repo_row[1])
-            if r.should_analyze():
-                r.download()
-                r.analyze()
-                r.yamlToCSV()
-            r.clean()
-        except Exception as e:
-            log.write(str(e))
-            log.write("\nMoving on to next repo...\n")
+        repo_row = None
+        for i, row in enumerate(reader):
+            if i == repo_index:
+                repo_row = row
+                break
 
+        if not repo_row:
+            raise Exception("Could not find row: {}".format(repo_index))
 
-def make_dirs(dirs):
-    for d in dirs:
-        try:
-            os.stat(d)
-        except:
-            os.mkdir(d)
+        r = Repo(repo_row)
+        r.download(log)
+        r.analyze(log)
+        r.yamlToCSV(log)
 
-
-def start_log(log_dir):
-    global log
-    log_path = os.path.join(log_dir, 'gitalizo_log_{}'.format(int(time.time())))
-    log = open(log_path, 'a+')
-    log.write("Started at {}\n".format(time.ctime()))
+    except Exception as e:
+        log.add(str(e))
+        sys.exit(1)
 
 
-def end_log():
-    log.write("Finished at {}\n".format(time.ctime()))
-    log.close()
+class Log:
+    def __init__(self):
+        file_name = 'gitalizo_log_{}'.format(int(time.time()))
+        self.file = open(file_name, 'a+')
+        self.add("Started Log")
 
-#Takes a YAML file output by Analizo (filename should be '<repoID>.yaml')
+    def end_log(self):
+        self.add("Finished Log".format(time.ctime()))
+        self.file.close()
+
+    def add(self, m):
+        self.file.write("{}: {}\n".format(time.ctime(), m))
+
+
+# Takes a YAML file output by Analizo (filename should be '<repoID>.yaml')
 def AnalizoToSQLCSV(analizoMetricsFile):
     fileLines = open(analizoMetricsFile).readlines()
     repoID = analizoMetricsFile[:-5]
 
     repoMetrics = []
-    lineIndex = 1 #skip first '---' line
+    lineIndex = 1  # skip first '---' line
     line = fileLines[lineIndex]
-    while ('---' not in line):        
+    while ('---' not in line):
         splitLine = line.split(':')
-        metric = splitLine[1].strip() 
+        metric = splitLine[1].strip()
         repoMetrics.append(metric)
         lineIndex += 1
         line = fileLines[lineIndex]
 
     WriteRepoCSVFile(repoMetrics, repoID)
-    
-#Writes repo-level metrics as a row to repoMetrics.csv (appending or creating if it does not already exist)
+
+
+# Writes repo-level metrics as a row to repoMetrics.csv
+# (appending or creating if it does not already exist)
 def WriteRepoCSVFile(metricsList, repoID):
-    repoMetricsFile = os.path.join(CSV_DIR, 'repoMetrics.csv')
-    
+    repoMetricsFile = 'repoMetrics.csv'
+
     CSVrow = repoID + ','
     for metric in metricsList:
         CSVrow += metric + ','
-    CSVrow = CSVrow[:-1] #strip the trailing comma
-    
+    CSVrow = CSVrow[:-1]  # strip the trailing comma
+
     CSVfile = open(repoMetricsFile, 'a+')
     CSVfile.write(CSVrow + '\n')
-    
+
     CSVfile.close()
-    
+
+
 class Repo:
-    ANALIZO_OUTS = 'analizo_outs'
-    REPO_TEMP = 'repo_temp'
     SSH_TEMPLATE = 'git@github.com:{}.git'
 
-    def __init__(self, repo_id, repo_url):
-        self.id = repo_id
-        self.url = Repo.SSH_TEMPLATE.format(urlparse(repo_url).path[7:])
-        self.clone_dir = os.path.join(Repo.REPO_TEMP, self.id)
-        self.out_file = os.path.join(Repo.ANALIZO_OUTS, self.id)
+    def __init__(self, repo_row):
+        self.id = repo_row[0]
+        self.url = Repo.SSH_TEMPLATE.format(urlparse(repo_row[1]).path[7:])
 
-    def download(self):
-        make_dirs([self.clone_dir])
-        log.write("Cloning repo {}\n".format(self.id))
+    def download(self, log):
+        log.add("Cloning repo {}".format(self.id))
+        cmd = ['git', 'clone', self.url, '.']
+        log.add(subprocess.check_output(cmd))
+        log.add("Done cloning")
 
-        cmd = ['git', 'clone', self.url, self.clone_dir]
+    def analyze(self, log):
+        log.add("Analyzing repo {}".format(self.id))
+        cmd = ['analizo', 'metrics', '.', '-o', self.id]
+        log.add(subprocess.check_output(cmd))
+        log.add("Done analyzing")
 
-        log.write(subprocess.check_output(cmd))
-        log.write("Done cloning\n")
+    def yamlToCSV(self, log):
+        log.add("Converting Analizo's YAML to CSV for repo {}".format(self.id))
+        AnalizoToSQLCSV(self.id)
+        log.add("Done converting")
 
-    def analyze(self):
-        log.write("Analyzing repo {}\n".format(self.id))
-
-        cmd = ['analizo', 'metrics', self.clone_dir, '-o', self.out_file]
-
-        log.write(subprocess.check_output(cmd))
-        log.write("Done analyzing\n")
-    
-    def clean(self):
-        log.write("Cleaning up after repo {}\n".format(self.id))
-
-        cmd = ['rm', '-rf', self.clone_dir]
-
-        log.write(subprocess.check_output(cmd))
-        log.write("Done cleaning up\n")
-
-    def yamlToCSV(self):
-        log.write("Converting Analizo's YAML to CSV for repo {}\n".format(self.id))
-        AnalizoToSQLCSV(self.out_file)
-        log.write("Done converting\n")
-        
-    def should_analyze(self):
-        # placeholder for smarter function to weed out bad repos
-        if os.path.isfile(self.out_file):
-            log.write("Skipping - {} already analyzed".format(self.id))
-            return False
-        return True
 
 if __name__ == '__main__':
     main()
--- install_analizo.sh
+++
@@ -1,7 +0,0 @@
-echo deb http://www.analizo.org/download/ ./ >> /etc/apt/sources.list.d/analizo.list
-echo deb-src http://www.analizo.org/download/ ./ >> /etc/apt/sources.list.d/analizo.list
-apt-get update
-yes Y | apt-get install analizo
-wget -O - http://www.analizo.org/download/signing-key.asc | apt-key add -
-apt-get update
-yes Y | apt-get install analizo
